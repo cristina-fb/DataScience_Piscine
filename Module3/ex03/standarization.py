10686a263cf8700dba6f7ab6712732230658eb5e
cristina-fb <cristina_fb_26@hotmail.es>
2025-04-18 17:56:15 +0200

    M3 ex03 done but need to check standarization values

---
+++ Module3/ex03/standarization.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+
+if __name__ == '__main__':
+    df_test = pd.read_csv('../Test_knight.csv')
+    df_train = pd.read_csv('../Train_knight.csv')
+
+    header = list(df_test.columns)
+    for col in header:
+        df_test[col] = (df_test[col] - df_test[col].mean()) / df_test[col].std()
+
+    header = list(df_train.columns)
+    header.remove('knight')
+    for col in header:
+        df_train[col] = (df_train[col] - df_train[col].mean()) / df_train[col].std()
+
+    print(df_test)
+    print('---------------------------------------------------')
+    print(df_train)
+
+
+    jedi = df_train[df_train['knight'] == 'Jedi'].drop('knight', axis='columns')
+    sith = df_train[df_train['knight'] == 'Sith'].drop('knight', axis='columns')
+
+    fig, ax = plt.subplots(1,2, figsize=(10, 5))
+    jedi.plot.scatter('Empowered','Stims',ax=ax[0], alpha=0.42, color='blue', label='Jedi')
+    sith.plot.scatter('Empowered','Stims',ax=ax[0], alpha=0.42, color='red', label='Sith')
+    df_test.plot.scatter('Empowered','Stims',ax=ax[1], alpha=0.42, color='green', label='knight')
+    # plt.show()
+    plt.savefig('standarized.jpg')
+    # TODO check values of standarized
