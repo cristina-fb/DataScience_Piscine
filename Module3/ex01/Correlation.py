6c5bbfc3fbaf7769c62fc12e4fa5308c440a0734
cristina-fb <cristina_fb_26@hotmail.es>
2025-04-18 15:58:32 +0200

    M3 ex01 done but need to check knight corr values

---
+++ Module3/ex01/Correlation.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    df = pd.read_csv('../Train_knight.csv')
+    df.loc[df['knight'] == 'Jedi', 'knight'] = 1
+    df.loc[df['knight'] == 'Sith', 'knight'] = -1
+    correlation = df.corr()['knight']
+    correlation = correlation.sort_values(ascending=False)
+    print(correlation)
+# TODO Check values of Jedi and Sith
