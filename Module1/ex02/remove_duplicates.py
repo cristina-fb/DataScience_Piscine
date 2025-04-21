e86bf970d97a6e969c0e473357c5f5974a7324a2
Cristina Fernandez baldovinos <cristina_fb_26@hotmail.es>
2025-04-21 11:21:52 +0200

    M1 ex02 finally removing dups

--- Module1/ex02/remove_duplicates.py
+++ Module1/ex02/remove_duplicates.py
@@ -2,14 +2,16 @@ import psycopg2, os, re
 from pathlib import Path
 
 def remove_dups():
-    tmp_table_cmd = """CREATE TABLE rmv_temp AS 
-                    SELECT *, LAG(event_time) OVER (ORDER BY event_time ASC) AS last_timestamp, 
-                    ROW_NUMBER() OVER (PARTITION BY event_type, product_id, price, user_id, user_session ORDER BY event_time ASC) AS RowNum 
-                    FROM customers"""
-    non_dups_cmd = """CREATE TABLE unique_tmp AS
-                    SELECT * FROM rmv_temp WHERE 
-                    RowNum = 1 OR ((RowNum > 1) AND (event_time > (last_timestamp + interval '1 second')))"""
-    drop_tables_cmd = """DROP TABLE customers, rmv_temp"""
+    tmp_table_cmd = """CREATE TABLE unique_tmp AS
+                        WITH rmv_temp AS( 
+                        SELECT *, 
+                        ROW_NUMBER() OVER (PARTITION BY event_type, product_id, price, user_id, user_session ORDER BY event_type, product_id, price, user_id, user_session, event_time ASC) AS RowNum,
+                        LAG(event_time) OVER (ORDER BY event_type, product_id, price, user_id, user_session, event_time ASC) AS last_timestamp 
+                        FROM customers)
+                        SELECT event_time, event_type, product_id, price, user_id, user_session FROM rmv_temp WHERE 
+                        RowNum = 1 OR ((RowNum > 1) AND (event_time > (last_timestamp + interval '1 second')))
+                        ORDER BY event_time"""
+    drop_table_cmd = """DROP TABLE customers"""
     rename_cmd = """ALTER TABLE unique_tmp RENAME TO customers"""
 
     connection = None
@@ -24,8 +26,7 @@ def remove_dups():
         connection = psycopg2.connect(**pgConnectionData)
         cur = connection.cursor()
         cur.execute(tmp_table_cmd)
-        cur.execute(non_dups_cmd)
-        cur.execute(drop_tables_cmd)
+        cur.execute(drop_table_cmd)
         cur.execute(rename_cmd)
         cur.close()
         connection.commit()
